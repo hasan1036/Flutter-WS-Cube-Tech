260a64c16680bf89f8ca7145e46115124c84ad9f
Hasan <bdhasan009@gmail.com>
2023-02-15 16:37:59 +0600

    40.1

--- lib/main.dart
+++ lib/main.dart
@@ -28,29 +28,7 @@ class DashBoardScreen extends StatelessWidget{
          width: double.infinity,
          height: double.infinity,
          color: Colors.blue.shade50,
-         child: Center(
-           child: Container(
-             width: 100,
-             height: 100,
-             decoration: BoxDecoration(
-               color: Colors.red,
-              // borderRadius: BorderRadius.circular(21),
-               border: Border.all(
-                 width: 2,
-                 color: Colors.black
-               ),
-               boxShadow: [
-                 BoxShadow(
-                   blurRadius: 11,
-                   spreadRadius: 8,
-                   color: Colors.grey
-                 )
-               ],
-               shape: BoxShape.circle
 
-             ),
-           ),
-         ),
        ) ,
      );
    }
